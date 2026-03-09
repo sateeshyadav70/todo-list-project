2ad30fb05e7b57e297c81137d20f8a374f80d8d0
sateesh kumar <sateeshkumardoc@gmail.com>
2026-03-09 21:12:58 +0530

    in this  time i am create a add task form and update task form and apply style on  both after this update i am work database

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 import express from 'express';
 import path from 'path';
+import { MongoClient } from 'mongodb';
 
 const app=express();
+
+const dbname="node-project";
+const collectionName="todo-list";
+const url="mongodb://localhost:27017";
+const client=new MongoClient(url);
+
+const connection = async()=>{
+  const connect= await client.connect();
+  return await connect.db(dbname)
+  
+}
 
 const publicPath=path.resolve("public");
 app.use(express.static(publicPath))
 
 app.set("view engine","ejs")
+app.use(express.urlencoded({extended:false}))
 
 app.get("/",(req,res)=>{
   res.render("list");
@@ -17,10 +30,19 @@ app.get("/add",(req,res)=>{
 app.get("/update",(req,res)=>{
   res.render("update");
 })
-app.post("/add",(req,res)=>{
+app.post("/add",async (req,res)=>{
+  const db= await connection();
+  const collection=db.collection(collectionName)
+  const result=collection.insertOne(req.body);
+  if(result){
+     res.redirect("/");
+  }else{
+    
   res.redirect("/");
+  }
+
 })
 app.post("/update",(req,res)=>{
   res.redirect("/")
 })
-app.listen(3000)
+app.listen(3300)
