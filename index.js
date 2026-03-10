4922a5ecdd8664eedf6793d4675a63c5dddcc859
sateesh kumar <sateeshkumardoc@gmail.com>
2026-03-10 21:01:25 +0530

    i am working on task listing in this session i show data from data base on ui and working how to delete data

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import path from 'path';
-import { MongoClient } from 'mongodb';
+import { MongoClient, ObjectId } from 'mongodb';
 
 const app=express();
 
@@ -21,8 +21,12 @@ app.use(express.static(publicPath))
 app.set("view engine","ejs")
 app.use(express.urlencoded({extended:false}))
 
-app.get("/",(req,res)=>{
-  res.render("list");
+app.get("/",async (req,res)=>{
+  const db= await connection();
+  const collection=db.collection(collectionName);
+  const result=await collection.find().toArray();
+  console.log(result)
+  res.render("list",{result});
 })
 app.get("/add",(req,res)=>{
   res.render("add");
@@ -33,7 +37,8 @@ app.get("/update",(req,res)=>{
 app.post("/add",async (req,res)=>{
   const db= await connection();
   const collection=db.collection(collectionName)
-  const result=collection.insertOne(req.body);
+  const result= await collection.insertOne(req.body);
+  console.log(req.body);
   if(result){
      res.redirect("/");
   }else{
@@ -43,6 +48,24 @@ app.post("/add",async (req,res)=>{
 
 })
 app.post("/update",(req,res)=>{
+
+  res.redirect("/")
+})
+app.get("/delete/:id",async (req,res)=>{
+  const db= await connection();
+  const collection=db.collection(collectionName)
+  const result= await collection.deleteOne({_id:new ObjectId(req.params.id)});
+  console.log(req.body);
+  if(result){
+     res.redirect("/");
+  }else{
+    
+  res.redirect("/");
+  }
+
+})
+app.post("/update",(req,res)=>{
+
   res.redirect("/")
 })
 app.listen(3300)
